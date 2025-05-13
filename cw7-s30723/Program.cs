5ca217ebf3b5cbf5ed25a753bab06eb8760a510b
extrem1k <lukkor10@gmail.com>
2025-05-13 20:47:32 +0200

    Update Program.cs

--- cw7-s30723/Program.cs
+++ cw7-s30723/Program.cs
@@ -3,17 +3,17 @@ using TravelAgencyAPI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
 builder.Services.AddControllers();
 builder.Services.AddScoped<ITravelService, TravelService>();
 
-// Configure Swagger/OpenAPI
+
 builder.Services.AddEndpointsApiExplorer();
 
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 
 
 app.UseHttpsRedirection();
